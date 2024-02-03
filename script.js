eec2515993875eace1934e8424a8a429185b07ca
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-02-03 20:21:57 +0100

    add: roll dice functionality

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Hello World!');
+'use strict';
+
+// Selecting elements
+const score1Paragraph = document.getElementById('score--1');
+const score2Paragraph = document.getElementById('score--2');
+const diceImage = document.querySelector('.dice');
+const newGameBtn = document.querySelector('.btn--new');
+const rollDiceBtn = document.querySelector('.btn--roll');
+const storeScoreBtn = document.querySelector('.btn--store');
+const currentScoreP1 = document.getElementById('current--1');
+const currentScoreP2 = document.getElementById('current--2');
+
+let currentScore = 0;
+
+// Initial state
+score1Paragraph.textContent = 0;
+score2Paragraph.textContent = 0;
+diceImage.classList.add('hidden');
+
+rollDiceBtn.addEventListener('click', () => {
+	const randomNmb = Math.trunc(Math.random() * 6) + 1;
+
+	diceImage.classList.remove('hidden');
+	diceImage.src = `assets/dice-${randomNmb}.png`;
+
+	if (randomNmb !== 1) {
+		currentScore += randomNmb;
+		currentScoreP1.textContent = currentScore;
+	} else {
+		currentScore = 0;
+		currentScoreP1.textContent = currentScore;
+	}
+});
