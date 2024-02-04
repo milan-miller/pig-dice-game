84ad005ad3a9d6a29c90a7c5421c8a1f8a241eeb
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-02-04 20:58:24 +0100

    add: store score functionality

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // Selecting elements
-const score1Paragraph = document.getElementById('score--1');
-const score2Paragraph = document.getElementById('score--2');
 const diceImage = document.querySelector('.dice');
 const newGameBtn = document.querySelector('.btn--new');
 const rollDiceBtn = document.querySelector('.btn--roll');
@@ -12,7 +10,7 @@ const currentScoreP2 = document.getElementById('current--2');
 const p1Active = document.querySelector('.p1--active');
 const p2Active = document.querySelector('.p2--active');
 
-const scores = {
+const playerScores = {
 	p1: 0,
 	p2: 0,
 };
@@ -20,9 +18,23 @@ const scores = {
 let currentScore = 0;
 let activePlayer = 1;
 
+const switchActivePlayer = () => {
+	currentScore = 0;
+	document.getElementById(`current--${activePlayer}`).textContent =
+		currentScore;
+
+	activePlayer = activePlayer === 1 ? 2 : 1;
+
+	if (activePlayer === 2) {
+		p1Active.classList.toggle('hidden');
+		p2Active.classList.toggle('hidden');
+	} else {
+		p2Active.classList.toggle('hidden');
+		p1Active.classList.toggle('hidden');
+	}
+};
+
 // Initial state
-score1Paragraph.textContent = 0;
-score2Paragraph.textContent = 0;
 diceImage.classList.add('hidden');
 
 rollDiceBtn.addEventListener('click', () => {
@@ -36,17 +48,21 @@ rollDiceBtn.addEventListener('click', () => {
 		document.getElementById(`current--${activePlayer}`).textContent =
 			currentScore;
 	} else {
-		currentScore = 0;
-		document.getElementById(`current--${activePlayer}`).textContent =
-			currentScore;
-		activePlayer = activePlayer === 1 ? 2 : 1;
-
-		if (activePlayer === 2) {
-			p1Active.classList.toggle('hidden');
-			p2Active.classList.toggle('hidden');
-		} else {
-			p2Active.classList.toggle('hidden');
-			p1Active.classList.toggle('hidden');
-		}
+		switchActivePlayer();
 	}
+});
+
+storeScoreBtn.addEventListener('click', () => {
+	playerScores[`p${activePlayer}`] += currentScore;
+
+	document.getElementById(`score--${activePlayer}`).textContent =
+		playerScores[`p${activePlayer}`];
+
+	if (playerScores[`p${activePlayer}`] >= 100) {
+		console.log(`Player ${activePlayer} won!`);
+	}
+
+	diceImage.classList.add('hidden');
+
+	switchActivePlayer();
 });
